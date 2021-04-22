2378dc8b2b570d86bfe68995326a1760dad92313
ammarsaeed-beejo <ammarsaeed.art@gmail.com>
2021-04-22 05:05:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const color=prompt('enter color',);
+
+switch(color){
+
+    case("red"): alert("apple");
+
+    break;
+
+    case("yellow"): alert("banana");
+
+    break;
+
+    case("green"): alert("grass");
+
+    break;
+
+    case("square"): case("rectangle"): case("circle"):
+
+    alert("shape");
+
+    break;
+
+    default:
+        alert("i dont know");
+}
